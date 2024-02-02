0bfdb6c16ce4d279a5483055e7ee30415cedc28c
mcsordi <matheuscarissordi@gmail.com>
2024-02-02 20:30:51 -0300

    fix express require

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dovenv = require("dotenv").config();
 const cors = require("cors");
+const app = express();
 app.use(express.json());
 app.use(cors());
 mongoose.connect(process.env.BASE_URL);
